372e5be9e481e1e8eb3fb53a41c10ee0d7662f51
Rywells88 <37821625+Rywells88@users.noreply.github.com>
2021-05-14 15:23:29 -0400

    Title screen works

---
+++ world/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  world
+//
+//  Created by Ryley Wells on 2021-05-14.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    
+}
